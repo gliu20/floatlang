b75b73ea695b0e498e248a0e31ee36dda8b3a0f2
George <24921711+gliu20@users.noreply.github.com>
2022-02-02 00:27:18 +0000

    add test list for getAtIndex

--- bootstrap/exponential-skip-list.js
+++ bootstrap/exponential-skip-list.js
@@ -1,7 +1,7 @@
 //// MEMORY ////
 
 const NULL = 0;
-const memory = [];
+let memory = [];
 
 // bump allocator
 const malloc = (function () {
@@ -22,7 +22,7 @@ const free = (function () {
 })
 
 const deref = (index) => {
-    if (index === NULL)
+    if (index === NULL || index === null)
         throw new Error ("Error: segmentation fault");
     return memory[index];
 }
@@ -51,6 +51,44 @@ struct skipListNode = {
 }
 */
 
+const testSkipList = [
+    //// list header ////
+    null,           // 0 segmentation fault 
+    5,              // 1 length of list
+    3,              // 2 HEAD ptr
+
+    //// node 0 ////
+    5,              // 3 size
+    "0th ele",      // 4 value
+    8,              // 5 lvl1Ptr // node 1
+    12,             // 6 Lvl2Ptr // node 2
+    19,             // 7 Lvl3Ptr // node 4
+
+    //// node 1 ////
+    4,              // 8 size
+    "1st ele",      // 9 value
+    12,             // 10 lvl1Ptr // node 2
+    16,             // 11 lvl2Ptr // node 3
+
+    //// node 2 ////
+    4,              // 12 size
+    "2nd ele",      // 13 value
+    16,             // 14 lvl1Ptr // node 3
+    19,             // 15 lvl2Ptr // node 4
+
+    //// node 3 ////
+    3,              // 16 size
+    "3rd ele",      // 17 value
+    19,             // 18 lvl1Ptr // node 4
+
+    //// node 4 ////
+    3,              // 19 size
+    "4th ele",      // 20 value
+    NULL,           // 21 lvl1Ptr // TAIL ptr
+];
+
+memory = testSkipList;
+
 // returns pointer to skipListNode
 const getAtIndex = (skipListHeader, index) => {
     const length = deref(skipListHeader);
@@ -66,7 +104,7 @@ const getAtIndex = (skipListHeader, index) => {
         const value = deref(currPtr + 1);
         const distFromDest = index - i;
 
-        console.log(`Info: looking at ${value} at i: ${i}`);
+        console.log(`Info: looking at '${value}' at i: ${i} w/ dist: ${distFromDest}`);
 
         for (let j = 0; j < nodeLength - 2; j++) {
             const nodeFieldIndex = nodeLength - 1 - j;
@@ -76,6 +114,7 @@ const getAtIndex = (skipListHeader, index) => {
 
             if (skipAmount <= distFromDest &&
                 nextPtr !== NULL) {
+                    console.log(`Info: Skipping ${skipAmount} units forward`)
                     currPtr = nextPtr;
                     i += skipAmount;
             }
@@ -83,4 +122,6 @@ const getAtIndex = (skipListHeader, index) => {
             if (index === i) return currPtr;
         }
     }
+
+    return NULL;
 }
