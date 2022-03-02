471107d55cedc2b0c432aeca82bea50eaa4a6988
George <24921711+gliu20@users.noreply.github.com>
2022-03-02 20:15:00 +0000

    make tokenizer more readable

--- bootstrap/tokenizer.js
+++ bootstrap/tokenizer.js
@@ -44,108 +44,38 @@ const tokenize = (str) => {
                 pushToken();
                 currPartialToken = "";
                 break;
-            case "!":
-            case "\"":
-            case "#":
-            case "$":
-            case "%":
-            case "&":
-            case "'":
-            case "(":
-            case ")":
-            case "*":
-            case "+":
-            case ",":
-            case "-":
-            case ".":
-            case "/":
-            case ":":
-            case ";":
-            case "<":
-            case "=":
-            case ">":
-            case "?":
-            case "@":
-            case "[":
-            case "\\":
-            case "]":
-            case "^":
-            case "_":
-            case "`":
-            case "{":
-            case "|":
+
+            case "!": case "\"": case "#": case "$": case "%":
+            case "&": case "'": case "(": case ")": case "*":
+            case "+": case ",": case "-": case ".": case "/":
+            case ":": case ";": case "<": case "=": case ">":
+            case "?": case "@": case "[": case "\\": case "]":
+            case "^": case "_": case "`": case "{": case "|":
             case "}":
+
                 pushToken();
                 pushChar(currChar);
                 currPartialToken = "";
 
                 break;
             // nums
-            case "0":
-            case "1":
-            case "2":
-            case "3":
-            case "4":
-            case "5":
-            case "6":
-            case "7":
-            case "8":
-            case "9":
+            case "0": case "1": case "2": case "3": case "4":
+            case "5": case "6": case "7": case "8": case "9":
 
             // uppercase letters
-            case "A":
-            case "B":
-            case "C":
-            case "D":
-            case "E":
-            case "F":
-            case "G":
-            case "H":
-            case "I":
-            case "J":
-            case "K":
-            case "L":
-            case "M":
-            case "N":
-            case "O":
-            case "P":
-            case "Q":
-            case "R":
-            case "S":
-            case "T":
-            case "U":
-            case "V":
-            case "W":
-            case "X":
-            case "Y":
+            case "A": case "B": case "C": case "D": case "E":
+            case "F": case "G": case "H": case "I": case "J":
+            case "K": case "L": case "M": case "N": case "O":
+            case "P": case "Q": case "R": case "S": case "T":
+            case "U": case "V": case "W": case "X": case "Y":
             case "Z":
 
             // lowercase letters
-            case "a":
-            case "b":
-            case "c":
-            case "d":
-            case "e":
-            case "f":
-            case "g":
-            case "h":
-            case "i":
-            case "j":
-            case "k":
-            case "l":
-            case "m":
-            case "n":
-            case "o":
-            case "p":
-            case "q":
-            case "r":
-            case "s":
-            case "t":
-            case "u":
-            case "v":
-            case "w":
-            case "x":
-            case "y":
+            case "a": case "b": case "c": case "d": case "e":
+            case "f": case "g": case "h": case "i": case "j":
+            case "k": case "l": case "m": case "n": case "o":
+            case "p": case "q": case "r": case "s": case "t":
+            case "u": case "v": case "w": case "x": case "y":
             case "z":
                 currPartialToken += currChar;
                 break;
